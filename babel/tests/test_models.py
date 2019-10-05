92482c78c69fd0408a3504416b9bc2a101daef48
totobgycs <totobgycs@gmail.com>
2019-10-05 16:10:17 +0200

    Test refactoring

--- babel/tests/test_models.py
+++ babel/tests/test_models.py
@@ -5,34 +5,34 @@ from unittest.mock import patch
 
 from babel.models import Translation
 
-from babel.tests.fixtures import fixture_languages, fixture_messages
-from babel.tests.utils import consts
+from babel.tests.fixtures import FixtureLanguages, FixtureMessages
+from babel.tests.utils import Consts
 
 class test_translation_model(TestCase):
 
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        fixture_languages.init_languages()
-        fixture_messages.init_messages()
+        FixtureLanguages.init_languages()
+        FixtureMessages.init_messages()
 
       
     @patch('babel.models.get_google_translation')    
     def test_translation_get_translated_message_different_languges(self, mock_translator):
 
-        mock_translator.return_value = consts.TRANSLATED_TEXT
-        translation_result_1 = Translation.get_translated_message(fixture_messages.MESSAGE, fixture_languages.TARGET_LANGUAGE)
-        translation_result_2 = Translation.get_translated_message(fixture_messages.MESSAGE, fixture_languages.TARGET_LANGUAGE)
+        mock_translator.return_value = Consts.TRANSLATED_TEXT
+        translation_result_1 = Translation.get_translated_message(FixtureMessages.MESSAGE, FixtureLanguages.TARGET_LANGUAGE)
+        translation_result_2 = Translation.get_translated_message(FixtureMessages.MESSAGE, FixtureLanguages.TARGET_LANGUAGE)
 
-        self.assertEqual(translation_result_1, consts.TRANSLATED_TEXT)
-        self.assertEqual(translation_result_2, consts.TRANSLATED_TEXT)
+        self.assertEqual(translation_result_1, Consts.TRANSLATED_TEXT)
+        self.assertEqual(translation_result_2, Consts.TRANSLATED_TEXT)
         mock_translator.assert_called_once()
 
     @patch('babel.models.get_google_translation')    
     def test_translation_get_translated_message_same_languge(self, mock_translator):
 
-        mock_translator.return_value = consts.TRANSLATED_TEXT
-        translation_result = Translation.get_translated_message(fixture_messages.MESSAGE, fixture_languages.SOURCE_LANGUAGE)
+        mock_translator.return_value = Consts.TRANSLATED_TEXT
+        translation_result = Translation.get_translated_message(FixtureMessages.MESSAGE, FixtureLanguages.SOURCE_LANGUAGE)
 
-        self.assertEqual(translation_result, fixture_messages.MESSAGE.text_native)
+        self.assertEqual(translation_result, FixtureMessages.MESSAGE.text_native)
         mock_translator.assert_not_called()
